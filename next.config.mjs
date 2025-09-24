c6d0a74cf5c60f694bb4db47e10bb526430b9dc9
SideeID <sandipratama164026@gmail.com>
2025-09-24 10:43:48 +0700

    feat: add redirects configuration to route root path to /pesanan

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,38 @@
 /** @type {import("next").NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/pesanan",
+        permanent: false,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.sanity.io",
-        port: ""
+        port: "",
       },
       {
         protocol: "https",
         hostname: "lh3.googleusercontent.com",
-        port: ""
+        port: "",
       },
       {
         protocol: "https",
         hostname: "avatars.githubusercontent.com",
-        port: ""
+        port: "",
       },
       {
         protocol: "https",
         hostname: "pub-b7fd9c30cdbf439183b75041f5f71b92.r2.dev",
-        port: ""
-      }
-    ]
-  }
+        port: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;

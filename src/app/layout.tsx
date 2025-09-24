8f8c86d5a805ce5d67ea7d323f485eeea86b5fd4
SideeID <sandipratama164026@gmail.com>
2025-09-24 10:51:37 +0700

    feat: update branding references from NextAdmin to Manganin in metadata, user info, and logo components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,11 +14,11 @@ import MuiXLicense from "@/components/MuiXLicense";
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | NextAdmin - Next.js Dashboard Kit",
-    default: "NextAdmin - Next.js Dashboard Kit",
+    template: "%s | Manganin - Dashbaord",
+    default: "Manganin - Dashboard",
   },
   description:
-    "Next.js admin dashboard toolkit with 200+ templates, UI components, and integrations for fast dashboard development.",
+    "Manganin dashboard restaurant admin panel to manage orders, menu, and customers.",
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
